e743d0709b7301c91b2ccbbb8e3457c0b1f66b9a
sriiu <sriiu@yahoo.co.in>
2023-01-25 22:05:07 -0500

    `

--- ec2host/c1-version.tf
+++ ec2host/c1-version.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
   # Adding Backend as S3 for Remote State Storage
- /* backend "s3" {
+   backend "s3" {
     bucket = "terraformstatefiless"
     key    = "dev/terraform.tfstate"
     region = "us-east-1" 
@@ -16,8 +16,6 @@ terraform {
     # For State Locking
     dynamodb_table = "terraform_dev"    
   }  
-}
-*/
 
 
 provider "aws" {
